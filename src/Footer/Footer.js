b5a2375df696d2e9fac3f6c6edf681ee2e49b02d
Sapphiris <milin091500@gmail.com>
2023-09-21 15:29:33 -0700

    footer added

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import { ReactComponent as Lemon } from "../images/lemon.svg";
 
 const Footer = () => {
-	return <footer className="footer"></footer>;
+	return (
+		<footer className="footer">
+			<div className="footer-divide-1">
+				<Lemon className="lemon" />
+			</div>
+			<div className="footer-divide-2">
+				<h3>Contact</h3>
+				<p>(123) 456 - 7890</p>
+				<p>123 Lemon Drive</p>
+				<p>Lemone, CA 94000</p>
+				<p>contact@lemon.com</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
